df2c5ae75b42119cb8387e52236aa11c526e0b60
edil3ra <edil3ra@gmail.com>
2025-08-19 13:53:13 +0200

    t

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 #![allow(unused_imports)]
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-               stream.write_all(b"+PONG\r\n");
+                loop {
+                    let mut buf = [0; 512];
+                    let read_count = stream.read(&mut buf[..]).unwrap();
+                    if read_count == 0 {
+                        break;
+                    }
+                    stream.write_all(b"+PONG\r\n").unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
