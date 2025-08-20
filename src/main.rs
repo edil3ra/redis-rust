0268aa688a79b64d66817c37655521d173bc14d5
edil3ra <edil3ra@gmail.com>
2025-08-20 23:08:53 +0200

    feat: Implement SET with PX and expiration logic for GET

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,113 @@
 mod resp;
 
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, sync::Arc, time::Duration};
 
 use anyhow::Result;
 use resp::Value;
 use tokio::{
     net::{TcpListener, TcpStream},
     sync::Mutex,
+    time::Instant,
 };
 
-#[tokio::main]
-async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-    let pairs: Arc<Mutex<HashMap<String, String>>> = Arc::new(Mutex::new(HashMap::new()));
+struct Db {
+    values: HashMap<String, String>,
+    expirations: HashMap<String, Instant>,
+}
 
-    loop {
-        let stream = listener.accept().await;
-        let pairs_by_stream = pairs.clone();
-        match stream {
-            Ok((stream, _add)) => {
-                tokio::spawn(async move { handle_conn(stream, pairs_by_stream).await });
-            }
-            Err(e) => {
-                println!("{e}");
+enum DbGetResult {
+    Ok(String),
+    KeyMissing,
+    Expired,
+}
+
+impl Db {
+    fn new() -> Self {
+        Self {
+            values: HashMap::new(),
+            expirations: HashMap::new(),
+        }
+    }
+
+    fn insert(&mut self, k: String, v: String, millis: Option<u64>) {
+        if let Some(millis) = millis {
+            self.expirations
+                .insert(k.clone(), Instant::now() + Duration::from_millis(millis));
+        }
+
+        self.values.insert(k, v);
+    }
+
+    fn get(&mut self, key: &str) -> DbGetResult {
+        if let Some(value) = self.values.get(key) {
+            if let Some(instant) = self.expirations.get(key) {
+                if Instant::now() >= *instant {
+                    return DbGetResult::Expired;
+                } else {
+                    return DbGetResult::Ok(value.into());
+                }
+            } else {
+                return DbGetResult::Ok(value.into());
             }
         }
+        DbGetResult::KeyMissing
     }
 }
 
-// *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n
-
-async fn handle_conn(stream: TcpStream, pairs: Arc<Mutex<HashMap<String, String>>>) {
+async fn handle_conn(stream: TcpStream, db: Arc<Mutex<Db>>) -> Result<usize> {
     let mut handler = resp::RespHandler::new(stream);
 
     loop {
         let value = handler.read_value().await.unwrap();
         let response = if let Some(v) = value {
             let (command, args) = extract_command(v).unwrap();
-            match command.to_lowercase().as_str() {
-                "ping" => Value::SimpleString("PONG".to_string()),
-                "echo" => args.first().unwrap().clone(),
-                "set" => {
+            match command.to_uppercase().as_str() {
+                "PING" => Value::SimpleString("PONG".to_string()),
+                "ECHO" => args.first().unwrap().clone(),
+                "SET" => {
                     let key = args.first().unwrap().clone();
                     let value = args.get(1).unwrap().clone();
-                    pairs.lock().await.insert(key.into(), value.into());
+                    let px: Option<String> = args.get(2).map(|px| px.clone().into());
+                    let millis_string: Option<String> = args.get(3).map(|px| px.clone().into());
+                    let millis_u64 = if let Some(px) = px {
+                        if px.as_str().to_uppercase() == "PX" {
+                            if let Some(m) = millis_string {
+                                m.parse::<u64>().ok()
+                            } else {
+                                panic!("Missing millis");
+                                // return Err(anyhow::anyhow!("Missing millis"));
+                            }
+                        } else {
+                            panic!("Missing PX");
+                            // return Err(anyhow::anyhow!("Missing PX "));
+                        }
+                    } else {
+                        None
+                    };
+
+                    db.lock().await.insert(key.into(), value.into(), millis_u64);
                     Value::SimpleString("OK".to_string())
                 }
-                "get" => {
+                "GET" => {
                     let key: String = args.first().unwrap().clone().into();
-                    let value = pairs.lock().await.get(&key).unwrap().clone();
-                    Value::SimpleString(value.to_string())
+                    let value = db.lock().await.get(&key);
+                    match value {
+                        DbGetResult::Ok(value) => Value::SimpleString(value.to_string()),
+                        DbGetResult::KeyMissing => {
+                            panic!("Key missing");
+                        }
+                        DbGetResult::Expired => Value::BulkString("".to_string()),
+                    }
                 }
                 c => panic!("Cannot handle command {c}"),
             }
         } else {
             break;
         };
-
-        println!("Sending value {response:?}");
-
         handler.write_value(response).await.unwrap();
     }
+
+    Ok(1)
 }
 
 fn extract_command(value: Value) -> Result<(String, Vec<Value>)> {
@@ -79,3 +126,24 @@ fn unpack_bulk_str(value: Value) -> Result<String> {
         _ => Err(anyhow::anyhow!("Expected command to be a bulk string")),
     }
 }
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    let db: Arc<Mutex<Db>> = Arc::new(Mutex::new(Db::new()));
+
+    loop {
+        let stream = listener.accept().await;
+        let db_for_stream = db.clone();
+        match stream {
+            Ok((stream, _add)) => {
+                tokio::spawn(async move { handle_conn(stream, db_for_stream).await });
+            }
+            Err(e) => {
+                println!("{e}");
+            }
+        }
+    }
+}
+
+// *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n
